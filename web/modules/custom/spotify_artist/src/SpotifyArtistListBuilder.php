84e710657f6283ccea9797cc880dd21f972bd9c9
Colin Stillwell <itsme@colinstillwell.com>
2025-02-22 23:21:20 +0000

    [master] feat: introduce custom image styles

--- web/modules/custom/spotify_artist/src/SpotifyArtistListBuilder.php
+++ web/modules/custom/spotify_artist/src/SpotifyArtistListBuilder.php
@@ -62,7 +62,7 @@ class SpotifyArtistListBuilder extends EntityListBuilder {
    */
   public function buildRow(EntityInterface $entity): array {
     /** @var \Drupal\spotify_artist\Entity\SpotifyArtistInterface $entity */
-    $row['artist_image'] = Markup::create($entity->getArtistImage('thumbnail'));
+    $row['artist_image'] = Markup::create($entity->getArtistImage('spotify_artist_80'));
     $row['artist_name'] = $entity->getArtistName();
     $row['page_title'] = $entity->toLink();
     $row['artist_genres'] = $entity->getArtistGenres();
